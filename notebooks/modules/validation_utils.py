58d81aaa23c76027d0fca9fd2cfa1d701a118309
riga <r.gmencia@gmail.com>
2024-04-22 17:35:45 +0100

    Moves code to files

--- notebooks/modules/validation_utils.py
+++
@@ -1,8 +0,0 @@
-@staticmethod
-def is_mono_ascending(values) -> bool:
-    return all(x < y for x, y in zip(values, values[1:]))
-
-
-@staticmethod
-def are_over(values, low=0) -> bool:
-    return all(low < x for x in values)
